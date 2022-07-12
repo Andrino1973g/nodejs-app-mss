2c24c9bc9e7c4027a3b91f513f04f0ccea030de8
Nnamdi Okeke <57027783+Andrino1973g@users.noreply.github.com>
2022-07-12 08:11:39 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.228.17.64:9000/admin/users',
+  serverUrl: 'http://18.234.233.41:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '319e749b303f153faffcb95d4e7f58800eb3f0b3',
+	    'sonar.login': 'c0a4fa41676e7cab559a5340459c26be2dde56a7',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
